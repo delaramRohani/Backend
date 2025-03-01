675e2d7b0fff8f1c87067e55d0f7935c4a3423b4
Delaram Rohani <dlrmrhi80@gmail.com>
2025-03-01 15:17:06 +0330

    Write code to get token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization");
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ message: "Access denied! No token provided." });
-  }
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
 
-  try {
-    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Invalid token!" });
-  }
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: "Invalid token" });
+
+        req.user = user;
+        next();
+    });
 };
 
-module.exports = authMiddleware;
+module.exports = { authenticateToken };
