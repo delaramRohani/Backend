5fd4db92e5643d5513397a9bcc8cc6802e4fa970
Delaram Rohani <dlrmrhi80@gmail.com>
2025-03-01 15:16:29 +0330

    add new auth to show user profile

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express();
+const { authenticateToken } = require("../middlewares/authMiddleware");
+const User = require("./users");
+
+router.get("/me", authenticateToken, async (req, res) => {
+    try {
+        const user = await User.findByPk(req.user.id, {
+            attributes: ["id", "name", "email"],
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+module.exports = router;
