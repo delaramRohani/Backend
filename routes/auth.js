7c4bdc5a3e90cfacf5c193ee3017f6e9ca62a87d
Delaram Rohani <dlrmrhi80@gmail.com>
2025-03-01 15:28:56 +0330

    change th authMiddleware path.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express();
-const { authenticateToken } = require("../middlewares/authMiddleware");
+const { authenticateToken } = require("../middleware/authMiddleware");
 const User = require("./users");
 
 router.get("/me", authenticateToken, async (req, res) => {
